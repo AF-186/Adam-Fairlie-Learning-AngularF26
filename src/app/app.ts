d141481ffa8a58b9258c05e5233dbe508b05f959
bub-9394 <257834996+bub-2521@users.noreply.github.com>
2026-09-22 20:19:14 -0400

    Final step of Assignment 2: - Created an HTML template for the Fish data type
    - Implemented @for loop
      - Supports my optional variables
      - Includes an @empty
    
    - Didn't quite meet the 15-line SCSS bonus mark (Changed 12 lines if you include @media declaration)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Fish } from './shared/models/fish';
 
 @Component({
   imports: [RouterOutlet],
@@ -7,8 +8,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
+
 export class App {
-  protected readonly title = signal('Adam-Fairlie-Learning-AngularF26');
-  name:string = 'Adam Fairlie';
-  week:number = 2;
-}
+  title = 'FFXIV Fish Log';
+
+  fishList: Fish[] = [
+    { id: 1, name: 'Fish 1', location: 'Place', weather: 'Raining', time: '22:00' }, // FFXIV uses 24-hour time notation
+    { id: 2, name: 'Fish 2', location: 'Place' }, // Testing out optional variables
+    { id: 3, name: "Fish 3", location: "Place 2", time: "12:00" }, // Testing out different quotes, union alternates
+    { id: 4, name: 'Fish 4', location: 'Place 2' },
+    { id: 5, name: 'Fish 5', location: 'Place 3' },
+    { id: 6, name: 'Fish 6', location: 'Place 4' },
+  ];
+} // Don't even know if this is what the assignment instructions meant
